47fb36b3689ac569ecc998d34eab7267dbcc14e9
Alaa Hajjaj <alaa.hajjaj@laratech.io>
2023-05-23 10:12:02 +0200

    communication from front to back

--- chat-ui/src/app/app.component.ts
+++ chat-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
+import { ChatService} from './service/chat.service';
 
 @Component({
  selector: 'app-root',
@@ -7,6 +8,9 @@ import {Component, ElementRef, ViewChild} from '@angular/core';
 })
 export class AppComponent {
  title = 'chat-ui';
+
+ constructor(private chatService: ChatService) {
+}
 
  @ViewChild('chatListContainer') list?: ElementRef<HTMLDivElement>;
  chatInputMessage: string = "";
@@ -30,19 +34,57 @@ export class AppComponent {
    },
  ];
 
+ ngOnInit() {
+  if (localStorage.getItem('fakeUserId') === null) {
+    localStorage.setItem('fakeUserId', this.generateFakeId())
+  } else {
+    this.chatService.findAll().subscribe(data => {
+      if (data !== null) {
+        this.setConversation(data['conversation'])
+      }
+    });
+  }
+ }
+
+ setConversation(conversation: any) {
+  for (let i = 0; i < conversation.length; i++) {
+    if (i % 2 === 0) {
+      this.chatMessages.push({
+        message: conversation[i],
+        user: this.human
+      });
+    } else {
+      this.chatMessages.push({
+        message: conversation[i],
+        user: this.bot
+      });
+    }
+  }
+ }
+
  ngAfterViewChecked() {
    this.scrollToBottom()
  }
 
  send() {
-   this.chatMessages.push({
-     message: this.chatInputMessage,
-     user: this.human
-   });
+ this.chatMessages.push({
+   message: this.chatInputMessage,
+   user: this.human
+ });
+ this.chatService.send(this.chatInputMessage).subscribe(data => {
+   this.receive(data);
+ });
+ this.chatInputMessage = ""
+ this.scrollToBottom()
+}
 
-   this.chatInputMessage = ""
-   this.scrollToBottom()
- }
+receive(message: string) {
+ this.chatMessages.push({
+   message: message,
+   user: this.bot
+ });
+ this.scrollToBottom()
+}
 
  scrollToBottom() {
    const maxScroll = this.list?.nativeElement.scrollHeight;
@@ -56,6 +98,14 @@ export class AppComponent {
  }
 
  clearConversation() {
-
+  localStorage.removeItem('fakeUserId')
+  localStorage.setItem('fakeUserId', this.generateFakeId())
+  this.chatService.deleteConversation()
+  this.chatMessages = [
+    {
+      user: this.bot,
+      message: "hi, I'm an AI. You can start any conversation..."
+    },
+  ];
  }
 }
